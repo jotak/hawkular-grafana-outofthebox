6596e28a09b0a386fa8688a3d692b616e09d6fdc
Joel Takvorian <jtakvori@redhat.com>
2017-05-31 15:07:17 +0200

    Star dashboards - rename cpu to load-avg

--- cpudemo/push-cpu.sh
+++ cpudemo/push-cpu.sh
@@ -13,7 +13,7 @@ do
   cpu=`top -bn1 | grep load | awk -F', |: ' '{printf $(NF-2)}'`
   ux_timestamp=`date +%s`
   timestamp=$(($ux_timestamp * 1000))
-  curl -u jdoe:password -X POST http://hawkular:8080/hawkular/metrics/gauges/cpu/raw \
+  curl -u jdoe:password -X POST http://hawkular:8080/hawkular/metrics/gauges/load-avg/raw \
     -d "[{\"timestamp\": $timestamp, \"value\": $cpu}]" \
     -H "Content-Type: application/json" -H "Hawkular-Tenant: $TENANT"
   sleep 10
