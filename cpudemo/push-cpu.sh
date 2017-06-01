50dafe06bb814bc9fc27d257022459a2107e6964
Joel Takvorian <jtakvori@redhat.com>
2017-06-01 10:17:50 +0200

    Improve push-cpu: read from /proc/load-avg

--- cpudemo/push-cpu.sh
+++ cpudemo/push-cpu.sh
@@ -10,11 +10,11 @@ done
 echo "Starting CPU-push script"
 while :
 do
-  cpu=`top -bn1 | grep load | awk -F', |: ' '{printf $(NF-2)}'`
+  loadavg=`awk '{ print $1 }' /proc/loadavg`
   ux_timestamp=`date +%s`
   timestamp=$(($ux_timestamp * 1000))
   curl -u jdoe:password -X POST http://hawkular:8080/hawkular/metrics/gauges/load-avg/raw \
-    -d "[{\"timestamp\": $timestamp, \"value\": $cpu}]" \
+    -d "[{\"timestamp\": $timestamp, \"value\": $loadavg}]" \
     -H "Content-Type: application/json" -H "Hawkular-Tenant: $TENANT"
   sleep 10
 done
