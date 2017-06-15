1b95ce89595726e08ebc07eb872c6453901b62a6
Joel Takvorian <jtakvori@redhat.com>
2017-06-15 15:28:21 +0200

    Sed to new files to overcome FS issues in kube

--- bootstrap/bootstrap-grafana.sh
+++ bootstrap/bootstrap-grafana.sh
@@ -9,12 +9,12 @@ do
 done
 
 echo "Creating hawkular datasource for tenant $TENANT"
-sed -i "s/\$TENANT/$TENANT/g" new-datasource.json
+sed -e "s/\$TENANT/$TENANT/g" new-datasource.json > new-datasource-edited.json
 
-curl -u admin:admin -H "Content-Type: application/json" -X POST -d @new-datasource.json http://grafana:3000/api/datasources
+curl -u admin:admin -H "Content-Type: application/json" -X POST -d @new-datasource-edited.json http://grafana:3000/api/datasources
 
 for dashboard in ${DASHBOARDS}; do
     echo "Importing dashboard $dashboard"
-    sed -i "s/\$TENANT/$TENANT/g" $dashboard.json
-    curl -u admin:admin -H "Content-Type: application/json" -X POST -d @$dashboard.json http://grafana:3000/api/dashboards/db
+    sed -e "s/\$TENANT/$TENANT/g" $dashboard.json > $dashboard-edited.json
+    curl -u admin:admin -H "Content-Type: application/json" -X POST -d @$dashboard-edited.json http://grafana:3000/api/dashboards/db
 done
